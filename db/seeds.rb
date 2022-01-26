8451ef5f633d36514ec8f6b343835fdfc96c1374
shineve <shineve95@gmail.com>
2022-01-26 23:22:54 +0800

    added fake data generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  company = Company.new
+  company.name = Faker::Company.name
+  company.code = Faker::Company.russian_tax_number
+  company.description = Faker::Lorem.paragraphs(number: rand(4..8)).map { |pr| "<p>#{pr}</p>" }.join
+  company.mission = Faker::Lorem.paragraphs(number: rand(4..8)).map { |pr| "<p>#{pr}</p>" }.join
+  company.benefits = Faker::Lorem.paragraphs(number: rand(4..8)).map { |pr| "<p>#{pr}</p>" }.join
+  company.save
+end
+
+companies = Company.all
+companies.each do |company|
+  rand(5..15).times do
+    company.jobs.create(
+      title: Faker::Job.title,
+      company_id: company.id,
+      description: Faker::Lorem.paragraphs(number: rand(4..8)).map { |pr| "<p>#{pr}</p>" }.join,
+      requirements: Faker::Lorem.paragraphs(number: rand(4..8)).map { |pr| "<p>#{pr}</p>" }.join,
+      employee_benefits: Faker::Lorem.paragraphs(number: rand(4..8)).map { |pr| "<p>#{pr}</p>" }.join,
+      min_salary: Faker::Number.between(from: 100_000, to: 150_000),
+      max_salary: Faker::Number.between(from: 200_000, to: 300_000),
+      currency: 'TWD'
+    )
+  end
+end
